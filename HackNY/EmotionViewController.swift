3733622b144dc502ff5f6a963d057886e463deee
NashidC <nash.chowdhury@gmail.com>
2017-10-15 03:05:27 -0400

    just finished basics frameworks

---
+++ HackNY/EmotionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmotionViewController.swift
+//  HackNY
+//
+//  Created by Nashid  on 10/14/17.
+//  Copyright © 2017 Nashid Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class EmotionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
